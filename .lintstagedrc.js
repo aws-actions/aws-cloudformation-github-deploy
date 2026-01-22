af4f80c7f14d5c6465bd7c0617aa20f86af88306
Kevin DeJong <kddejong@amazon.com>
2026-01-22 11:41:15 -0800

    Add .prettierignore and fix linting issues

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,10 @@
 module.exports = {
-    // Prettier
-    '**/*.md': ['prettier --ignore-path .gitignore --write'],
-  
-    // Eslint
-    '**/*.{ts,tsx}': ['eslint --fix'],
-  
-    // Jest
-    '**/*.test.{ml,mli,mly,ts,js,json}': 'jest',
-  }
-  
+  // Prettier
+  '**/*.md': ['prettier --ignore-path .gitignore --write'],
+
+  // Eslint
+  '**/*.{ts,tsx}': ['eslint --fix'],
+
+  // Jest
+  '**/*.test.{ml,mli,mly,ts,js,json}': 'jest'
+}
